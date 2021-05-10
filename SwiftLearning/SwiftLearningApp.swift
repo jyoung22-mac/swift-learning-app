33dbb82d861b9ac5a8a2230a13c3f2da25fc5a8e
Justin Young <justinyoung371@yahoo.com>
2021-05-09 20:46:55 -0700

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Justin Young on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
